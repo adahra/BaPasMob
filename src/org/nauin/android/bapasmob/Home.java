1ec97210ad4854a05e198dff69c97559da798d8e
nauin <adnantoa@gmail.com>
2015-02-27 08:06:57 +0700

    Initial Commit

---
+++ src/org/nauin/android/bapasmob/Home.java
@@ -0,0 +1,22 @@
+package org.nauin.android.bapasmob;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Home extends Fragment {
+
+	public Home() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View mView = inflater.inflate(R.layout.activity_home, container, false);
+		return mView;
+	}
+}
---
+++ src/org/nauin/android/bapasmob/Publikasi.java
@@ -0,0 +1,32 @@
+package org.nauin.android.bapasmob;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Publikasi extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View mView = inflater.inflate(R.layout.activity_publikasi, container,
+				false);
+		return mView;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
